4077d9f1e9e207ab99a00442f4cbd7addc58b812
Brandon <106548043+qwerty123873@users.noreply.github.com>
2025-01-18 15:55:00 -0500

    added some module files
    d ddddddddddddddddddddddddddddddddd

---
+++ src/main/java/frc/robot/subsystems/endEffector/EndEffector.java
@@ -0,0 +1,15 @@
+
+
+package frc.robot.subsystems.endEffector
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.RobotMode.RobotType;
+
+
+
+public class EndEffector extends  {
+    
+}
---
+++ src/main/java/frc/robot/subsystems/endEffector/EndEffectorIO.java
@@ -0,0 +1,14 @@
+
+package frc.robot.subsystems.endEffector;
+
+public interface EndEffectorIO {
+
+    public class EndEffectorIO {
+        double appledVolts = 0.0;
+        double appliedAmps = 0.0;
+        double endEffectorRPM = 0.0;
+        double endEffectorAngleDeg = 0.0;
+        
+    }
+
+}
