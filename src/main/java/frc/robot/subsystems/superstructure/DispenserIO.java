19e39761c03d84aab7256b660042e199dd804225
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2025-02-24 14:36:49 -0500

    Implement DispenserIO

---
+++ src/main/java/frc/robot/subsystems/superstructure/DispenserIO.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems.superstructure;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface DispenserIO {
+  @AutoLog
+  class DispenserIOInputs {
+    public boolean connected = false;
+
+    public double positionRads;
+    public double velocityRadsPerSec = 0.0;
+    public double appliedVoltage = 0.0;
+    public double currentAmps = 0.0;
+    public double tempCelsius = 0.0;
+
+    public boolean rearBeamBreakBroken = false;
+  }
+
+  default void updateInputs(DispenserIOInputs inputs) {}
+
+  default void runVolts(double output) {}
+
+  default void stop() {}
+
+  // default void setBrakeMode(boolean enabled) {}
+}
