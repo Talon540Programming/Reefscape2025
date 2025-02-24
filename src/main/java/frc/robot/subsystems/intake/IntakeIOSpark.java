1baa58d8dd96d97f443ec8aab682cd5d8d5466bf
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2025-02-24 11:09:27 -0500

    Add updatable debouncer

--- src/main/java/frc/robot/subsystems/intake/IntakeIOSpark.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSpark.java
@@ -8,7 +8,7 @@ import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
-import edu.wpi.first.math.filter.Debouncer;
+import frc.robot.util.Debouncer;
 
 public class IntakeIOSpark implements IntakeIO {
   private final SparkBase spark;
