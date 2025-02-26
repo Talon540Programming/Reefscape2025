b32b9fc75f2d4b8d5533fadc176c701eec4eff0d
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2025-02-26 18:48:48 -0500

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,17 +2,28 @@ package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.commands.DriveCommands;
+import frc.robot.commands.IntakeCommands;
+import frc.robot.subsystems.dispenser.DispenserBase;
+import frc.robot.subsystems.dispenser.DispenserIO;
+import frc.robot.subsystems.dispenser.DispenserIOSim;
+import frc.robot.subsystems.dispenser.DispenserIOSpark;
 import frc.robot.subsystems.drive.*;
+import frc.robot.subsystems.elevator.*;
 import frc.robot.subsystems.intake.IntakeBase;
 import frc.robot.subsystems.intake.IntakeIO;
 import frc.robot.subsystems.intake.IntakeIOSim;
 import frc.robot.subsystems.intake.IntakeIOSpark;
 import frc.robot.util.AllianceFlipUtil;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+import org.littletonrobotics.junction.networktables.LoggedNetworkNumber;
 
 public class RobotContainer {
   // Load PoseEstimator class
@@ -21,12 +32,19 @@ public class RobotContainer {
   // Subsystems
   private final DriveBase driveBase;
   private final IntakeBase intakeBase;
+  private final ElevatorBase elevatorBase;
+  private final DispenserBase dispenserBase;
 
   // Controller
   private final CommandXboxController controller = new CommandXboxController(0);
 
   // Dashboard inputs
   private final LoggedDashboardChooser<Command> autoChooser;
+
+  private final LoggedNetworkNumber endgameAlert1 =
+      new LoggedNetworkNumber("/SmartDashboard/Endgame Alert #1", 30.0);
+  private final LoggedNetworkNumber endgameAlert2 =
+      new LoggedNetworkNumber("/SmartDashboard/Endgame Alert #2", 15.0);
 
   public RobotContainer() {
     switch (Constants.getMode()) {
@@ -39,6 +57,8 @@ public class RobotContainer {
                 new ModuleIOSpark(2),
                 new ModuleIOSpark(3));
         intakeBase = new IntakeBase(new IntakeIOSpark());
+        elevatorBase = new ElevatorBase(new ElevatorIOSpark());
+        dispenserBase = new DispenserBase(new DispenserIOSpark());
       }
       case SIM -> {
         driveBase =
@@ -49,7 +69,8 @@ public class RobotContainer {
                 new ModuleIOSim(),
                 new ModuleIOSim());
         intakeBase = new IntakeBase(new IntakeIOSim());
-
+        elevatorBase = new ElevatorBase(new ElevatorIOSim());
+        dispenserBase = new DispenserBase(new DispenserIOSim());
       }
       default -> {
         driveBase =
@@ -60,6 +81,8 @@ public class RobotContainer {
                 new ModuleIO() {},
                 new ModuleIO() {});
         intakeBase = new IntakeBase(new IntakeIO() {});
+        elevatorBase = new ElevatorBase(new ElevatorIO() {});
+        dispenserBase = new DispenserBase(new DispenserIO() {});
       }
     }
 
@@ -72,6 +95,18 @@ public class RobotContainer {
           "Drive Wheel Radius Characterization", driveBase.wheelRadiusCharacterization());
       autoChooser.addOption(
           "Drive Simple FF Characterization", driveBase.feedforwardCharacterization());
+      autoChooser.addOption(
+          "Elevator Dynamic Forward",
+          elevatorBase.sysIdDynamic(SysIdRoutine.Direction.kForward, 0.5));
+      autoChooser.addOption(
+          "Elevator Dynamic Reverse",
+          elevatorBase.sysIdDynamic(SysIdRoutine.Direction.kReverse, 0.25));
+      autoChooser.addOption(
+          "Elevator Quasi Forward",
+          elevatorBase.sysIdQuasistatic(SysIdRoutine.Direction.kForward, 25));
+      autoChooser.addOption(
+          "Elevator Quasi Reverse",
+          elevatorBase.sysIdQuasistatic(SysIdRoutine.Direction.kReverse, 3));
     }
 
     configureButtonBindings();
@@ -84,9 +119,10 @@ public class RobotContainer {
             driveBase,
             () -> -controller.getLeftY(),
             () -> -controller.getLeftX(),
-            () -> -controller.getRightX()));
+            () -> -controller.getRightX(),
+            controller.leftStick()));
 
-    // Lock to 0° when A button is held
+    // TODO Lock to Feeder Station when held
     controller
         .a()
         .whileTrue(
@@ -94,14 +130,39 @@ public class RobotContainer {
                 driveBase,
                 () -> -controller.getLeftY(),
                 () -> -controller.getLeftX(),
-                () -> Rotation2d.kZero));
+                () -> Rotation2d.kZero,
+                controller.leftStick()));
 
-    // Switch to X pattern when X button is pressed
-    controller.x().onTrue(Commands.runOnce(driveBase::stopWithX, driveBase));
-
-    // Reset gyro to 0° when B button is pressed
+    // Stow
+    controller.povDown().onTrue(Commands.runOnce(() -> elevatorBase.setGoal(ElevatorState.STOW)));
+    // Intake
+    controller.b().onTrue(IntakeCommands.intake(elevatorBase, intakeBase, dispenserBase));
+    // L1
     controller
-        .b()
+        .povLeft()
+        .onTrue(Commands.runOnce(() -> elevatorBase.setGoal(ElevatorState.L1_CORAL)));
+    // L2
+    controller.povUp().onTrue(Commands.runOnce(() -> elevatorBase.setGoal(ElevatorState.L2_CORAL)));
+    // L3
+    controller
+        .povRight()
+        .onTrue(Commands.runOnce(() -> elevatorBase.setGoal(ElevatorState.L3_CORAL)));
+    // Dispense
+    controller.rightTrigger().onTrue(dispenserBase.eject());
+
+    // Home Elevator
+    controller.back().debounce(0.5).onTrue(elevatorBase.homingSequence());
+
+    // Auto Align (Left or Right)
+    // TODO
+
+    // Human Player Alert (Strobe LEDs)
+    // TODO
+
+    // Reset Gyro
+    controller
+        .start()
+        .and(controller.back())
         .onTrue(
             Commands.runOnce(
                     () ->
@@ -112,6 +173,29 @@ public class RobotContainer {
                                     AllianceFlipUtil.apply(new Rotation2d()))),
                     driveBase)
                 .ignoringDisable(true));
+
+    // Endgame
+    new Trigger(
+            () ->
+                DriverStation.isTeleopEnabled()
+                    && DriverStation.getMatchTime() > 0
+                    && DriverStation.getMatchTime() <= Math.round(endgameAlert1.get()))
+        .onTrue(
+            Commands.startEnd(
+                    () -> controller.setRumble(GenericHID.RumbleType.kBothRumble, 1.0),
+                    () -> controller.setRumble(GenericHID.RumbleType.kBothRumble, 0.0))
+                .withTimeout(0.5));
+
+    new Trigger(
+            () ->
+                DriverStation.isTeleopEnabled()
+                    && DriverStation.getMatchTime() > 0
+                    && DriverStation.getMatchTime() <= Math.round(endgameAlert2.get()))
+        .onTrue(
+            Commands.startEnd(
+                    () -> controller.setRumble(GenericHID.RumbleType.kBothRumble, 1.0),
+                    () -> controller.setRumble(GenericHID.RumbleType.kBothRumble, 0.0))
+                .withTimeout(0.5));
   }
 
   public Command getAutonomousCommand() {
