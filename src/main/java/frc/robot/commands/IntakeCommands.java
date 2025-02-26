1d3b19195f0965de2da490d954342cacf353d2dc
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2025-02-26 10:24:53 -0500

    add proto auto intake

---
+++ src/main/java/frc/robot/commands/IntakeCommands.java
@@ -0,0 +1,23 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.dispenser.DispenserBase;
+import frc.robot.subsystems.elevator.ElevatorBase;
+import frc.robot.subsystems.elevator.ElevatorState;
+import frc.robot.subsystems.intake.IntakeBase;
+import frc.robot.util.LoggedTunableNumber;
+
+public class IntakeCommands {
+  public static final LoggedTunableNumber intakeVolts =
+      new LoggedTunableNumber("Intake/HopperIntakeVolts", 6.0);
+
+  public static Command intake(ElevatorBase elevator, IntakeBase intake, DispenserBase dispenser) {
+    return Commands.runOnce(() -> elevator.setGoal(ElevatorState.INTAKE))
+        .alongWith(
+            Commands.waitUntil(elevator::isAtGoal)
+                .andThen(
+                    Commands.deadline(
+                        dispenser.intakeTillHolding(), intake.runRoller(intakeVolts.get()))));
+  }
+}
