a52aea8a3ea57bb5669199871c55c13e31986824
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2025-03-21 23:13:40 -0400

    Fix intake and dispenser gearing

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -3,5 +3,5 @@ package frc.robot.subsystems.intake;
 class IntakeConstants {
   public static final boolean inverted = true;
   public static final double moi = 0.025; // TODO
-  public static final double gearing = 2.0;
+  public static final double gearing = 23.0 / 12.0;
 }
