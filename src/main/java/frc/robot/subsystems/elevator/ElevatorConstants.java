bf781725a611cf1552fff7c569ec0ad30c4af1cc
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2025-03-22 00:05:09 -0400

    Update ElevatorConstants.java

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -9,14 +9,12 @@ public class ElevatorConstants {
   // Pitch from Elevator to Dispenser
   public static final Rotation2d dispenserPitch = Rotation2d.fromDegrees(61.6);
 
-  public static final double originToBaseHeightMeters =
-      Units.inchesToMeters(6.288); // <-- highest edge on slam plate
-  //    6.148 to the lowest edge, 6.218 to centroid
+  public static final double originToBaseHeightMeters = Units.inchesToMeters(6.288);
 
   public static final double drumRadius = Units.inchesToMeters(1.0);
   public static final double gearing = 4.0;
 
-  public static final double maxTravel = Units.inchesToMeters(41); // TODO
+  public static final double maxTravel = Units.inchesToMeters(72); // TODO
 
   public static final double carriageMassKg = Units.lbsToKilograms(6.0);
   public static final double stagesMassKg = Units.lbsToKilograms(12.0);
