20d2b518e6963ce106a2a6c501df334e3d47b815
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2025-04-02 15:43:50 -0400

    Expose gyro pitch and roll date

--- src/main/java/frc/robot/subsystems/drive/GyroIO.java
+++ src/main/java/frc/robot/subsystems/drive/GyroIO.java
@@ -8,8 +8,12 @@ public interface GyroIO {
   public static class GyroIOInputs {
     public boolean connected = false;
 
-    public Rotation2d yawPosition = new Rotation2d();
-    public double yawVelocityRadPerSec = 0.0;
+    public Rotation2d yawPosition;
+    public double yawVelocityRadPerSec;
+    public Rotation2d pitchPosition;
+    public double pitchVelocityRadPerSec;
+    public Rotation2d rollPosition;
+    public double rollVelocityRadPerSec;
 
     public Rotation2d[] odometryYawPositions = new Rotation2d[] {};
   }
