2c2548426c05ed9461aea889870d8faaac95addb
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2025-04-14 00:15:46 -0400

    Update ElevatorConstants.java

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -4,7 +4,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
-import frc.robot.util.LoggedTunableNumber;
 
 public class ElevatorConstants {
   // Measured from CAD
