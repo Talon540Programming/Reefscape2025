78ec654644e476cd4bb7f943253958a4eee2ecb8
AyaanMohammed <146781606+HollowKnight241@users.noreply.github.com>
2025-06-23 09:26:50 -0400

    j

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.subsystems.intake;
-
-public class IntakeConstants {
-  static final boolean inverted = true;
-  static final double moi = 0.025; // TODO
-  static final double gearing = 23.0 / 12.0;
-}
